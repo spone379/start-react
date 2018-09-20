dc16c27e1424c83f679ca168da0e38c6f800dab7
Vladyslav <spone379@gmail.com>
2018-09-20 13:45:25 +0300

    Add component for unmounting delay

---
+++ src/components/DelayedComponent/DelayedComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+// Use this HOC when we need animation on Unmount (with Modal)
+//
+// Example: 
+//     <DelayedComponent
+//       delayUnmountTime={500}                   :int
+//       isMount={this.state.mountSomeComponent}  :bool !
+//      >
+//          <SomeComponent />
+//      </DelayedComponent>
+class DelayedComponent extends Component {
+
+  state = {
+    shouldRender: this.props.isMount
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.isMount && !this.props.isMount) {
+      setTimeout(
+        () => this.setState({ shouldRender: false }),
+        this.props.delayUnmountTime || 500
+      );
+    } else if (!prevProps.isMount && this.props.isMount) {
+      this.setState({ shouldRender: true });
+    }
+  }
+
+  render() {
+    return this.state.shouldRender
+      ? this.props.children
+      : null;
+  }
+}
+
+export default DelayedComponent;
